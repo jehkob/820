b2cc2fd502b56abe260d91f2787734e7dd52ece3
Jakob Larry <15jrwl@queensu.ca>
2021-03-11 13:39:49 -0500

    adding XGB after finalizing RF information

---
+++ final project 820.R
@@ -0,0 +1,345 @@
+## Kaggle Datsaet
+
+spotify <- read.csv("C:/Users/Jakob/Documents/School/820/data.csv/data.csv")
+head(spotify)
+str(spotify)
+summary(spotify)
+
+
+spotify$id <- NULL
+
+## Libraries - EDA
+library(ggplot2)
+library(dplyr)
+library(corrplot)
+library(tidyr)
+library(janitor)
+
+any(is.na(spotify))
+# FALSE - no missing values
+
+numericSpotify <- c("acousticness","danceability","energy","instrumentalness","liveness","loudness","speechiness","tempo","valence")
+numCorr<-cor(spotify[,numericSpotify])
+corrplot(numCorr, method = "color")
+
+numIntSpotify <- c("acousticness","danceability","energy","instrumentalness","liveness","loudness","speechiness","tempo","valence", 
+                   "duration_ms","explicit","key","mode","popularity","year")
+numIntCorr <- cor(spotify[,numIntSpotify])
+corrplot(numIntCorr,method="shade", type = "upper")
+
+dfNumSpotify <- spotify[,numIntSpotify]
+par(mfrow=c(3,5))
+colnames <- dimnames(dfNumSpotify)[[2]]
+for (i in 1:15) {
+  d <- density(dfNumSpotify[,i])
+  plot(d, type = "n", main = colnames[i])
+  polygon(d, col="blue",border="gray")
+}
+  
+
+
+## PCA 
+library(ggfortify)
+spotifyPCA <- prcomp(dfNumSpotify, scale = TRUE, center =  TRUE, rank. = 5)
+summary(spotifyPCA) ## ~80% of variance can be explained by 9 components. 
+
+autoplot(spotifyPCA)
+
+## charts 
+
+spotifyByYear <- aggregate(acousticness~year, data=spotify, mean)
+ggplot(data=spotifyByYear, aes(x = year, y = acousticness)) + 
+  geom_line(size=1.5) +
+  ggtitle("Acousticness by Year") +
+  geom_smooth(method = lm, color = "red", se=TRUE, show.legend=TRUE, linetype ="longdash")
+
+## Acousticness has a neg corr w popularity (as years pass, it's a much less prevalent feature.
+spotifyByYear <- aggregate(energy~year, data=spotify, mean)
+ggplot(data=spotifyByYear, aes(x = year, y = energy)) + 
+  geom_line(size=1.5) +
+  ggtitle("Energy by Year") +
+  geom_smooth(method = lm, color = "red", se=TRUE, show.legend=TRUE, linetype ="longdash")
+
+spotifyByYear <- aggregate(tempo~year, data=spotify, mean)
+ggplot(data=spotifyByYear, aes(x = year, y = tempo)) + 
+  geom_line(size=1.5) +
+  ggtitle("Tempo by Year") +
+  geom_smooth(method = lm, color = "red", se=TRUE, show.legend=TRUE, linetype ="longdash")
+
+
+
+
+
+## pre-processing 
+# remove key, mode, duration_ms very little correlation (non-existent) to popularity 
+#remove release-date, name - irrelevant 
+
+
+spotify$name <- NULL 
+spotify$key <- NULL
+spotify$mode <- NULL
+spotify$duration_ms <- NULL
+spotify$release_date <- NULL
+spotify$artists <- NULL
+
+str(spotify) # leaves us w acousticness, artists, dance, energy, explicit, instr, liveness, loudness, pop, speechiness, tempo, valence, year
+
+
+#modeling 
+library(randomForest)
+library(randomForestExplainer)
+library(caret)
+library(e1071)
+library(tictoc)
+
+#First we perform 10-fold CV on entire dataset to find optimal values, then repeat splitting data in to train(70%) & test(30%)
+trainSample <- sample(1:nrow(spotify), 0.7 * nrow(spotify))
+spotifyTrain <- spotify[trainSample,]
+spotifyTest <- spotify[-trainSample,]
+
+
+# default baseline train
+tic()
+trControl <- trainControl(method="cv", search = "grid", number = 10)
+rfDefault <- train(popularity~., data = spotifyTrain1, method = "rf", metric = "RMSE",  trControl = trControl, tuneGrid = NULL)
+toc()
+
+#finding best mtry
+tic()
+set.seed(1)
+tuneGrid <- expand.grid(.mtry = c(1:11))
+rfMtry <- train(popularity~.,
+                data = spotifyTrain,
+                method = "rf",
+                metric = "RMSE",
+                tuneGrid = tuneGrid,
+                trControl = trControl,
+                importance = TRUE,
+                nodesize = 50,
+                ntree = 250)
+rfMtry
+
+mtryBest <- rfMtry$bestTune$mtry
+min(rfMtry$results$RMSE) 
+toc()
+
+## finding maxnodes - if highest value appears at 20 for the loop, we will reiterate through a larger set of values
+tic()
+maxNodeList <- list()
+tuneGrid <- expand.grid(.mtry = mtryBest)
+for (maxNode in c(5:20)){
+  set.seed(12)
+  rfMaxNode <- train(popularity~.,
+                     data=spotifyTrain,
+                     method = "rf",
+                     metric = "RMSE",
+                     tuneGrid = tuneGrid,
+                     trControl = trControl,
+                     importance = TRUE,
+                     nodesize = 50,
+                     maxnodes = maxNode,
+                     ntree = 250)
+  currentIter <- toString(maxNode)
+  maxNodeList[[currentIter]] <- rfMaxNode
+  
+}
+resMaxNode <- resamples(maxNodeList)
+summary(resMaxNode)
+toc()
+
+## reiterating through, as maxnodes was not high enough 
+tic()
+maxNodeList <- list()
+tuneGrid <- expand.grid(.mtry = mtryBest)
+for (maxNode in c(20:35)){
+  set.seed(12)
+  rfMaxNode <- train(popularity~.,
+                     data=spotifyTrain,
+                     method = "rf",
+                     metric = "RMSE",
+                     tuneGrid = tuneGrid,
+                     trControl = trControl,
+                     importance = TRUE,
+                     nodesize = 50,
+                     maxnodes = maxNode,
+                     ntree = 250)
+  currentIter <- toString(maxNode)
+  maxNodeList[[currentIter]] <- rfMaxNode
+  
+}
+resMaxNode <- resamples(maxNodeList)
+summary(resMaxNode)
+toc()
+
+## fluctuating RMSE & R-sqrd by ~32. marginal changes by that point forward
+
+
+
+## Find max trees - follows similar loop process
+tic()
+maxTreeList <- list()
+ for (ntree in c(350, 500, 750, 1000, 1250, 1500, 1750, 2000, 2500, 3000)) {
+   set.seed(1)
+   rfMaxTree <- train(popularity~., 
+                      data = spotifyTrain,
+                      method = "rf",
+                      metric = "RMSE",
+                      tuneGrid = tuneGrid,
+                      trControl = trControl,
+                      importance = TRUE,
+                      nodesize = 50,
+                      maxnodes = 32,
+                      ntree = ntree)
+   treeIter <- toString(ntree)
+   maxTreeList[[treeIter]] <- rfMaxTree
+ }
+resTree <- resamples(maxTreeList)
+summary(resTree)
+toc()
+
+## best fit 
+tic()
+tunedRF <- train(popularity~.,
+                 data = spotifyTrain,
+                 method = "rf",
+                 metric = "RMSE",
+                 tuneGrid = tuneGrid, 
+                 trControl = trControl,
+                 importance = TRUE,
+                 nodesize = 50,
+                 ntree = 1200,
+                 maxnodes = 32)
+toc()
+
+rfPred<- predict(tunedRF, newdata = spotifyTest)
+
+
+varImpPlot(tunedRF)
+tunedRFv2 <-randomForest(popularity~., data=spotifyTrain, mtry =6,nodesize=20,maxnodes=32,importance=TRUE,metric="RMSE",ntree=1250)
+varImpPlot(tunedRFv2)
+
+spotifyTrain2 <- spotifyTrain[,-12] #removes year
+## retry all the model tuning on this datset w/o year
+
+tic()
+set.seed(1)
+tuneGrid <- expand.grid(.mtry = c(1:11))
+rfMtry <- train(popularity~.,
+                data = spotifyTrain2,
+                method = "rf",
+                metric = "RMSE",
+                tuneGrid = tuneGrid,
+                trControl = trControl,
+                importance = TRUE,
+                nodesize = 50,
+                ntree = 250)
+rfMtry
+
+mtryBest <- rfMtry$bestTune$mtry
+min(rfMtry$results$RMSE) 
+toc()
+
+## finding maxnodes - if highest value appears at 20 for the loop, we will reiterate through a larger set of values
+tic()
+maxNodeList <- list()
+tuneGrid <- expand.grid(.mtry = mtryBest)
+for (maxNode in c(5:20)){
+  set.seed(12)
+  rfMaxNode <- train(popularity~.,
+                     data=spotifyTrain2,
+                     method = "rf",
+                     metric = "RMSE",
+                     tuneGrid = tuneGrid,
+                     trControl = trControl,
+                     importance = TRUE,
+                     nodesize = 50,
+                     maxnodes = maxNode,
+                     ntree = 250)
+  currentIter <- toString(maxNode)
+  maxNodeList[[currentIter]] <- rfMaxNode
+  
+}
+resMaxNode <- resamples(maxNodeList)
+summary(resMaxNode)
+toc()
+
+## reiterating through, as maxnodes was not high enough 
+tic()
+maxNodeList <- list()
+tuneGrid <- expand.grid(.mtry = mtryBest)
+for (maxNode in c(20:50)){
+  set.seed(12)
+  rfMaxNode <- train(popularity~.,
+                     data=spotifyTrain2,
+                     method = "rf",
+                     metric = "RMSE",
+                     tuneGrid = tuneGrid,
+                     trControl = trControl,
+                     importance = TRUE,
+                     nodesize = 50,
+                     maxnodes = maxNode,
+                     ntree = 250)
+  currentIter <- toString(maxNode)
+  maxNodeList[[currentIter]] <- rfMaxNode
+  
+}
+resMaxNode <- resamples(maxNodeList)
+summary(resMaxNode)
+toc()
+
+## fluctuating RMSE & R-sqrd by ~50. marginal changes by that point forward
+
+
+
+## Find max trees - follows similar loop process
+tic()
+maxTreeList <- list()
+for (ntree in c(350, 500, 750, 1000, 1250, 1500, 1750, 2000, 2500, 3000)) {
+  set.seed(1)
+  rfMaxTree <- train(popularity~., 
+                     data = spotifyTrain2,
+                     method = "rf",
+                     metric = "RMSE",
+                     tuneGrid = tuneGrid,
+                     trControl = trControl,
+                     importance = TRUE,
+                     nodesize = 50,
+                     maxnodes = 50,
+                     ntree = ntree)
+  treeIter <- toString(ntree)
+  maxTreeList[[treeIter]] <- rfMaxTree
+}
+resTree <- resamples(maxTreeList)
+summary(resTree)
+toc()
+
+## best fit 
+tic()
+tunedRFv3 <-randomForest(popularity~.,
+                                      data=spotifyTrain2,
+                                      mtry =6,
+                                      nodesize=50,
+                                      maxnodes=50,
+                                      importance=TRUE,
+                                      metric="RMSE",
+                                      ntree=2450)
+toc()
+varImpPlot(tunedRFv3)
+summary(tunedRFv3)
+
+rfPred2<- predict(tunedRFv3, newdata = spotifyTest)
+sqrt(mean((spotifyTest[,8] - rfPred2)^2))
+
+## XGBOOST TIME
+
+library(xgboost)
+library(doParallel)
+library(tidyverse)
+library(data.table)
+library(mlr)
+registerDoParallel(cores=8)
+
+#default xgboost before cv for model tuning
+
+
+
